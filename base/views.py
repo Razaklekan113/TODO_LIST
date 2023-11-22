bb467454bb00c7771acc175dd7c87e6d4bd77093
Razaklekan113 <razaklekan11@gmail.com>
2023-11-22 14:01:19 +0100

    new changes

--- base/views.py
+++ base/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Task
+from django.urls import reverse_lazy
 
 class TaskList(ListView):
     model = Task
@@ -11,3 +13,8 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = "task"
     template_name = "base/task.html"
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
